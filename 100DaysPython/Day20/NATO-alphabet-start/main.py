7e4e1541356363f2c79499b2a413b33f0caa7ae6
Umesh94kr <umeshkumar94628@gmail.com>
2026-01-10 16:01:53 +0530

    Day20 updated

--- 100DaysPython/Day20/NATO-alphabet-start/main.py
+++ 100DaysPython/Day20/NATO-alphabet-start/main.py
@@ -6,6 +6,8 @@ import pandas as pd
 BASE_DIR = os.path.dirname(__file__)
 CSV_PATH = os.path.join(BASE_DIR, 'nato_phonetic_alphabet.csv')
 
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+ 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 df = pd.read_csv(CSV_PATH)
 
@@ -13,6 +15,17 @@ letters_words = {row.letter : row.code for index, row in df.iterrows()}
 
 # 2. Create a list of the phonetic code words from a word that the user inputs.
 user_input = input("What is your name? ").upper()
-result = {alphabet : letters_words[alphabet] for alphabet in user_input}
+
+is_done = False
+while(is_done == False):
+    try:
+        result = {alphabet : letters_words[alphabet] for alphabet in user_input}
+        is_done = True
+    except KeyError as e:
+        print(f"Key not exists : {e}")
+        print(f"Input name should contain letters")
+        user_input = input("What is your name? ").upper()
+        is_done=False
+
 print(result)
 
